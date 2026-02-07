91093079899e29e8d4ec5fadf0ba963d956f77a6
harshgit1406 <harsh14raj1111@gmail.com>
2026-02-08 00:37:14 +0530

    backend deployement changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://hrms-lite-production-d126.up.railway.app';
 
 export const employeeAPI = {
   getAll: () => axios.get(`${API_URL}/employees/`),
