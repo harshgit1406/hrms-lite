73b3ee06cd279f337192589539e4fadc49f0dba8
harshgit1406 <harsh14raj1111@gmail.com>
2026-02-08 11:16:44 +0530

    fixed database

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://your-backend-url.up.railway.app';
+const API_URL = 'https://hrms-lite-production-d126.up.railway.app';
 //const API_URL = 'http://127.0.0.1:8000';
 
 export const employeeAPI = {
